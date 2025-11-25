7b04f76419bb6b2a3df36cccca23c76d8f2acadf
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-11-25 20:48:17 +0700

    Inisialisasi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,68 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mermaid from "astro-mermaid";
 import starlight from "@astrojs/starlight";
 import starlightThemeRapide from "starlight-theme-rapide";
+import starlightScrollToTop from "starlight-scroll-to-top";
 
 export default defineConfig({
+  site: "http://localhost:4321",
   integrations: [
+    mermaid({
+      // Default theme: 'default', 'dark', 'forest', 'neutral', 'base'
+      theme: "forest",
+      autoTheme: true,
+      mermaidConfig: {
+        flowchart: {
+          curve: "basis",
+        },
+      },
+      iconPacks: [
+        {
+          name: "logos",
+          loader: () =>
+            fetch("https://unpkg.com/@iconify-json/logos@1/icons.json").then(
+              (res) => res.json()
+            ),
+        },
+        {
+          name: "iconoir",
+          loader: () =>
+            fetch("https://unpkg.com/@iconify-json/iconoir@1/icons.json").then(
+              (res) => res.json()
+            ),
+        },
+      ],
+    }),
     starlight({
-      plugins: [starlightThemeRapide()],
-      title: "Dokumentasi",
+      plugins: [
+        starlightThemeRapide(),
+        starlightScrollToTop({
+          position: "right",
+          tooltipText: "Back to top",
+          showTooltip: false,
+          smoothScroll: true,
+          threshold: 10,
+          svgStrokeWidth: 1.5,
+          borderRadius: "20",
+          showProgressRing: true,
+          progressRingColor: "#011badff",
+          showOnHomepage: true,
+          svgPath: "M12 6L6 12M12 6L18 12M12 12L6 18M12 12L18 18",
+        }),
+      ],
+      title: {
+        id: "Dokumentasi",
+        en: "Documentation",
+        sn: "Pituduh",
+      },
+      defaultLocale: "root",
+      locales: {
+        root: { label: "Indonesia", lang: "id" },
+        en: { label: "English" },
+        sn: { label: "Sunda" },
+      },
+
       social: [
         {
           icon: "github",
@@ -84,4 +140,5 @@ export default defineConfig({
       ],
     }),
   ],
+  vite: { plugins: [tailwindcss()] },
 });
